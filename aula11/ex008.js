3aa39b189ca3c4893a2da13c8b6d7319d68f11a2
EricBerlim <ericberlim@gmail.com>
2022-01-31 19:38:42 -0300

    JS update

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+//CONDIÇÃO SIMPLES
+
+var vel = 12
+console.log(`A velocidade do carro é ${vel}Km/h.`)
+
+if (vel > 60) {
+    console.log(`Ultrapassou a velocidade! MULTADO!`)
+}
+
+console.log(`Use sempre o cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,14 @@
+//CONDIÇÃO COMPOSTA
+
+var país = 'Brasil'
+
+console.log(`Vivendo em ${país}`)
+
+if (país != 'Brasil') {
+    console.log('Estrangeiro(a)!')
+} else{
+    console.log('Brasileiro(a)!')
+}
+function action(params) {
+    
+}
