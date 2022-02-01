f193911998ebad7d07629564e5e69006c82deda3
EricBerlim <ericberlim@gmail.com>
2022-02-01 13:38:57 -0300

    JS - switch

---
+++ aula12/ex011.js
@@ -0,0 +1,14 @@
+//ESTRUTURAS CONDICIONAIS ANINHADAS
+
+var idade = 67
+console.log(`Você tem ${idade} anos.`)
+
+if (idade < 16) {
+    console.log('Não vota!')
+
+} else if (idade < 18) {
+    console.log('Voto opcional!')
+
+} else {
+    console.log('Voto obrigatório!')
+}
---
+++ aula12/ex013.js
@@ -0,0 +1,14 @@
+//SWITCH
+
+var agora = new Date()
+var diaSem = agora.getDay()
+
+/*
+0 - Domingo
+1 - 
+2 - 
+3 - 
+4 - 
+5 - 
+6 - 
+*/
