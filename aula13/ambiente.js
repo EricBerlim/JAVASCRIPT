2f290a4de0828017ccfe7577d85dce673188881d
EricBerlim <ericberlim@gmail.com>
2022-02-02 19:24:44 -0300

    JS - Estruturas de Repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+var cont = 1
+do {
+    console.log(`Passo ${cont}`)
+    cont++
+
+} while (cont <= 4)
+
+/*
+var cont = 1
+while (cont <= 4) {
+    console.log(`Passo ${cont}`)
+    cont++
+}
+*/
