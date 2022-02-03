6655605e4fe9b780ab94605798ec7e286625e677
EricBerlim <ericberlim@gmail.com>
2022-02-03 19:06:41 -0300

    JS - Recursividade

---
+++ aula16/function01.js
@@ -0,0 +1,10 @@
+function ParImpar(n) {
+    if (n % 2 == 0) {
+        return 'Par!'
+
+    } else {
+        return 'Ímpar!'
+    }
+}
+
+console.log(ParImpar(4))
---
+++ aula16/function02.js
@@ -0,0 +1,14 @@
+function soma(n1=0, n2=0) {
+    return n1 + n2    
+}
+
+console.log(soma(7, 8))
+
+
+/*Function dentro de variável:
+let v = function (x) {
+    return x*2
+}
+
+console.log(v(5))
+*/
