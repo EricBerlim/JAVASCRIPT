54193d4141ef8fe88eb8d18078d0e254fb49bb59
EricBerlim <ericberlim@gmail.com>
2022-02-03 13:57:45 -0300

    JS - tabuada

--- aula14x/ex017/script.js
+++ aula14x/ex017/script.js
@@ -1,3 +1,16 @@
-function calcular() {
-    
+function tabuada() {
+    let num = document.getElementById('txtN')
+    let tab = document.getElementById('selTab')
+
+    if (num.value.length == 0) {
+        alert('Digite um número, por favor!')
+
+    } else {
+        let n = Number(num.value)
+        let c = 1
+
+        while (c <= 10) {
+            
+        }
+    }
 }
