f42037058428d42facce60a06ad2fed360887909
EricBerlim <ericberlim@gmail.com>
2022-02-04 13:02:22 -0300

    JS - Criando Snippets e Functions

--- aula16x/script.js
+++ aula16x/script.js
@@ -1,6 +1,6 @@
-let num = document.getElementById('input#fNum')
-let lista = document.getElementById('select#fLista')
-let res = document.getElementById('div#res')
+let num = document.querySelector('input#fNum')
+let lista = document.querySelector('select#fLista')
+let res = document.querySelector('div#res')
 let valores = []
 
 //Se for um valor entre 1 e 100
@@ -33,6 +33,9 @@ function adicionar() {
         //Para adicionar na lista
         lista.appendChild(item)
 
+        //Limpa ao adicionar outro valor após finalizar
+        res.innerHTML = ''
+
     } else {
         alert('Valor inválido ou já adicionado!')
     }
@@ -41,4 +44,35 @@ function adicionar() {
     num.value = ''
     //Cursor volta a piscar na caixa input
     num.focus()
+}
+
+function finalizar() {
+    if (valores.value.length == 0) {
+        alert('Adicione valores antes de finalizar!')
+
+    } else {
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let média = 0
+
+        //Analisa 1 por 1 e verificar qual é o maior
+        for (let pos in valores){
+            soma += valores[pos]
+
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        média = soma / total
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${total} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>A soma foi ${soma}.</p>`
+        res.innerHTML += `<p>A média é ${média}.</p>`
+    }
 }
